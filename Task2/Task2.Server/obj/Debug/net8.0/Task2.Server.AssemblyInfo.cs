1e440b0933fa420308ba9298b609e29d596836ea
Farah-hatsuk <farah.hatsuk@gmail.com>
2025-03-16 23:33:28 +0300

    task

--- Task2/Task2.Server/obj/Debug/net8.0/Task2.Server.AssemblyInfo.cs
+++ Task2/Task2.Server/obj/Debug/net8.0/Task2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e453c1b0f5b48761f1eb639f5cbef03555b512c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
